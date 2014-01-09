5b18f6f94d74accb6891e5c5988075f6cec52836
Isaac Murchie <isaac@saucelabs.com>
2014-01-08 17:50:08 -0800

    Add utility class for generating HMAC auth token

---
+++ src/main/java/com/saucelabs/saucerest/SecurityUtils.java
@@ -0,0 +1,36 @@
+package com.saucelabs.common;
+
+import java.security.Key;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+/*
+ * Modelled after code from {@linkhttp://www.tomred.net/tutorials/tomred-java-generate-hmac-md5-sha1.html}
+*/
+public class SecurityUtils {
+    public static String hmacEncode(String algorithm, String input, String privateKey)
+        throws IllegalArgumentException {
+        try {
+            byte[] keyBytes = privateKey.getBytes();
+            Key key = new SecretKeySpec(keyBytes, 0, keyBytes.length, algorithm);
+            Mac mac = Mac.getInstance(algorithm);
+            mac.init(key);
+            return byteArrayToHex(mac.doFinal(input.getBytes()));
+        } catch(NoSuchAlgorithmException ex) {
+            throw new IllegalArgumentException("Unknown algorithm: " + algorithm);
+        }
+    }
+
+    protected static String byteArrayToHex(byte [] a) {
+        int hn, ln, cx;
+        String hexDigitChars = "0123456789abcdef";
+        StringBuffer buf = new StringBuffer(a.length * 2);
+        for(cx = 0; cx < a.length; cx++) {
+            hn = ((int)(a[cx]) & 0x00ff) / 16;
+            ln = ((int)(a[cx]) & 0x000f);
+            buf.append(hexDigitChars.charAt(hn));
+            buf.append(hexDigitChars.charAt(ln));
+        }
+        return buf.toString();
+    }
+}
