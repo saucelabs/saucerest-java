e5d488f9be775b2ca2874b9cfcee06b0ea1a862b
Gavin Mogan <gavin@saucelabs.com>
2015-11-29 22:46:18 -0800

    Remove some setters

--- src/main/java/com/saucelabs/saucerest/objects/Activity.java
+++ src/main/java/com/saucelabs/saucerest/objects/Activity.java
@@ -6,9 +6,6 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Set;
 
-/**
- * Created by halkeye on 2015-11-29.
- */
 public class Activity implements Serializable {
     public static class ActivityStats implements Serializable {
         @JsonProperty("in progress")
@@ -16,29 +13,19 @@ public class Activity implements Serializable {
 
         public int getInProgress() { return this.in_progress; }
 
-        public void setInProgress(int in_progress) { this.in_progress = in_progress; }
-
         @JsonProperty("all")
         private int all;
 
         public int getAll() { return this.all; }
 
-        public void setAll(int all) { this.all = all; }
-
         @JsonProperty("queued")
         private int queued;
 
         public int getQueued() { return this.queued; }
-
-        public void setQueued(int queued) { this.queued = queued; }
     }
 
     @JsonProperty("subaccounts")
     private HashMap<String, ActivityStats> subaccounts;
-
-    public void setSubaccounts(HashMap<String, ActivityStats> subaccounts) {
-        this.subaccounts = subaccounts;
-    }
 
     public Set<String> getSubaccounts() {
         return subaccounts.keySet();
@@ -53,7 +40,4 @@ public class Activity implements Serializable {
     private ActivityStats totals;
 
     public ActivityStats getTotals() { return this.totals; }
-
-    public void setTotals(ActivityStats totals) { this.totals = totals; }
-
 }
