2d31436a9de33dec6a2ecbdcc1ba521df78fe960
Yi Min Yang <yi-min.yang@saucelabs.com>
2023-01-03 16:36:14 +0100

    Turn off APAC tests

--- src/test/java/com/saucelabs/saucerest/integration/DataCenter.java
+++ src/test/java/com/saucelabs/saucerest/integration/DataCenter.java
@@ -3,8 +3,9 @@ package com.saucelabs.saucerest.integration;
 // Yes, duplicating instead of using DataCenter enum to restrict and control where these tests run.
 enum DataCenter {
     US("https://ondemand.us-west-1.saucelabs.com/wd/hub"),
-    EU("https://ondemand.eu-central-1.saucelabs.com/wd/hub"),
-    APAC_SOUTHEAST("https://ondemand.apac-southeast-1.saucelabs.com/wd/hub");
+    EU("https://ondemand.eu-central-1.saucelabs.com/wd/hub");
+    // Turn off testing against APAC as not all APIs are supported in this region yet
+    //APAC_SOUTHEAST("https://ondemand.apac-southeast-1.saucelabs.com/wd/hub");
 
     public final String label;
 
