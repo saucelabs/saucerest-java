37a0daaea68a24875fec6c58e6d894e4dd51d126
sceiler <sceiler@users.noreply.github.com>
2023-01-30 15:43:50 +0100

    Refactor createTeam()

--- src/main/java/com/saucelabs/saucerest/api/Accounts.java
+++ src/main/java/com/saucelabs/saucerest/api/Accounts.java
@@ -73,16 +73,42 @@ public class Accounts extends AbstractEndpoint {
         return getResponseClass(getResponseObject(url), Team.class);
     }
 
+    /**
+     * Creates a new team under the organization of the requesting account.
+     *
+     * @param name           A name for the new team.
+     * @param organizationID The unique ID of the organization under which the team is created. You can look up your organization ID by calling the GET https://api.{region}.saucelabs.com/team-management/v1/organizations/ endpoint.
+     * @param settings       The settings object specifies the concurrency allocations for the team within the organization. The available attributes are:
+     *                       virtual_machines - INTEGER
+     *                       The settings parameter is required, but you only need to include the applicable concurrency attribute(s) for the team.
+     * @return {@link CreateTeam}
+     * @throws IOException API request failed
+     */
     public CreateTeam createTeam(String name, String organizationID, Settings settings) throws IOException {
-        String url = getBaseEndpoint() + "teams/";
-        Map map = ImmutableMap.of("name", name, "organization", organizationID, "settings", settings);
-
-        return getResponseClass(postResponse(url, map), CreateTeam.class);
+        return createTeam(name, organizationID, settings, null);
     }
 
+    /**
+     * Creates a new team under the organization of the requesting account.
+     *
+     * @param name           A name for the new team.
+     * @param organizationID The unique ID of the organization under which the team is created. You can look up your organization ID by calling the GET https://api.{region}.saucelabs.com/team-management/v1/organizations/ endpoint.
+     * @param settings       The settings object specifies the concurrency allocations for the team within the organization. The available attributes are:
+     *                       virtual_machines - INTEGER
+     *                       The settings parameter is required, but you only need to include the applicable concurrency attribute(s) for the team.
+     * @param description    A description to distinguish the team within the organization.
+     * @return {@link CreateTeam}
+     * @throws IOException API request failed
+     */
     public CreateTeam createTeam(String name, String organizationID, Settings settings, String description) throws IOException {
         String url = getBaseEndpoint() + "teams/";
-        Map map = ImmutableMap.of("name", name, "organization", organizationID, "settings", settings, "description", description);
+        Map map;
+
+        if (description == null) {
+            map = ImmutableMap.of("name", name, "organization", organizationID, "settings", settings);
+        } else {
+            map = ImmutableMap.of("name", name, "organization", organizationID, "settings", settings, "description", description);
+        }
 
         return getResponseClass(postResponse(url, map), CreateTeam.class);
     }
