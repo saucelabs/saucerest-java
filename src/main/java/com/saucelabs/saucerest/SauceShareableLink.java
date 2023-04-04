0e5f4d9127a09b8660a92b67a157ee6babebd183
sceiler <sceiler@users.noreply.github.com>
2023-04-04 17:16:40 +0200

    Improve SauceShareableLink and write unit tests for it

--- src/main/java/com/saucelabs/saucerest/SauceShareableLink.java
+++ src/main/java/com/saucelabs/saucerest/SauceShareableLink.java
@@ -2,53 +2,82 @@ package com.saucelabs.saucerest;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-
-import static java.nio.charset.StandardCharsets.US_ASCII;
+import java.util.Formatter;
 
 /**
  * Class providing a method to create a shareable test results link of a test executed on Sauce Labs.
  */
 public class SauceShareableLink {
-    /**
-     * Based on the code from here: https://docs.saucelabs.com/test-results/sharing-test-results/index.html#example---java
-     * @param username Sauce Labs username
-     * @param accessKey Sauce Labs access key
-     * @param sauceJobId Sauce Labs job id
-     * @return Auth digest for the job, so it can be accessed without SauceLabs credentials.
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeyException
-     */
-    public static String getJobAuthDigest(String username, String accessKey, String sauceJobId) throws NoSuchAlgorithmException, InvalidKeyException {
-        String key = String.format("%s:%s", username , accessKey);
-        SecretKeySpec sks = new SecretKeySpec(key.getBytes(US_ASCII), "HmacMD5");
-        Mac mac = Mac.getInstance("HmacMD5");
-        mac.init(sks);
-        byte[] result = mac.doFinal(sauceJobId.getBytes(US_ASCII));
-        StringBuilder hash = new StringBuilder();
-        for (byte b : result) {
-            String hex = Integer.toHexString(0xFF & b);
-            if (hex.length() == 1) {
-                hash.append('0');
-            }
-            hash.append(hex);
+    private static String getJobAuthDigest(String username, String accessKey, String sauceJobId) {
+        try {
+            String key = String.format("%s:%s", username, accessKey);
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.US_ASCII), "HmacMD5");
+            Mac mac = Mac.getInstance("HmacMD5");
+            mac.init(secretKeySpec);
+            byte[] result = mac.doFinal(sauceJobId.getBytes(StandardCharsets.US_ASCII));
+            return bytesToHex(result).toLowerCase();
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new IllegalStateException("Error calculating job auth digest", e);
         }
-        return hash.toString();
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuffer hexStringBuffer = new StringBuffer();
+        Formatter formatter = new Formatter(hexStringBuffer);
+        for (byte b : bytes) {
+            formatter.format("%02x", b);
+        }
+        return hexStringBuffer.toString();
     }
 
     /**
      * Based on the code from here: https://docs.saucelabs.com/test-results/sharing-test-results/index.html#example---java
-     * @param username Sauce Labs username
-     * @param accessKey Sauce Labs access key
-     * @param sauceJobId Sauce Labs job id
-     * @param server Sauce Labs data center endpoint
+     *
+     * @param username           Sauce Labs username
+     * @param accessKey          Sauce Labs access key
+     * @param sauceJobId         Sauce Labs job id
+     * @param dataCenterEndpoint Sauce Labs data center endpoint
      * @return A url of the test result with an authentication token, so it can be accessed without Sauce Labs credentials.
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeyException
+     * @throws NoSuchAlgorithmException if the HmacMD5 algorithm is not available
+     * @throws InvalidKeyException      if the key is invalid
      */
-    public static String getShareableLink(String username, String accessKey, String sauceJobId, String server) throws NoSuchAlgorithmException, InvalidKeyException {
+    public static String getShareableLink(String username, String accessKey, String sauceJobId, String dataCenterEndpoint) throws NoSuchAlgorithmException, InvalidKeyException {
+        String defaultUsername = System.getenv("SAUCE_USERNAME");
+        String defaultAccessKey = System.getenv("SAUCE_ACCESS_KEY");
+
+        if (username == null || username.isEmpty()) {
+            if (defaultUsername == null || defaultUsername.isEmpty()) {
+                throw new IllegalArgumentException("Sauce Labs username cannot be null or empty");
+            } else {
+                username = defaultUsername;
+            }
+        }
+
+        if (accessKey == null || accessKey.isEmpty()) {
+            if (defaultAccessKey == null || defaultAccessKey.isEmpty()) {
+                throw new IllegalArgumentException("Sauce Labs access key cannot be null or empty");
+            } else {
+                accessKey = defaultAccessKey;
+            }
+        }
+
+        if (sauceJobId == null || sauceJobId.isEmpty()) {
+            throw new IllegalArgumentException("Sauce Labs job ID cannot be null or empty");
+        }
+
+        if (dataCenterEndpoint == null || dataCenterEndpoint.isEmpty()) {
+            throw new IllegalArgumentException("Sauce Labs data center endpoint cannot be null or empty");
+        }
+
         String digest = getJobAuthDigest(username, accessKey, sauceJobId);
-        return String.format("%s%s/%s?auth=%s", server, "tests", sauceJobId, digest);
+        StringBuilder builder = new StringBuilder(dataCenterEndpoint);
+        builder.append("tests/")
+            .append(sauceJobId)
+            .append("?auth=")
+            .append(digest);
+        return builder.toString();
     }
 }
--- src/test/java/com/saucelabs/saucerest/unit/SauceShareableLinkTest.java
+++ src/test/java/com/saucelabs/saucerest/unit/SauceShareableLinkTest.java
@@ -7,6 +7,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SauceShareableLinkTest {
 
@@ -17,15 +18,61 @@ public class SauceShareableLinkTest {
     private static final String SERVER = "https://saucelabs.com/rest/v1/";
 
     @Test
-    public void testGetJobAuthDigest() throws NoSuchAlgorithmException, InvalidKeyException {
-        String actualDigest = SauceShareableLink.getJobAuthDigest(USERNAME, ACCESS_KEY, JOB_ID);
-        assertEquals(EXPECTED_DIGEST, actualDigest);
-    }
-
-    @Test
     public void testGetShareableLink() throws NoSuchAlgorithmException, InvalidKeyException {
         String expectedLink = "https://saucelabs.com/rest/v1/tests/" + JOB_ID + "?auth=" + EXPECTED_DIGEST;
         String actualLink = SauceShareableLink.getShareableLink(USERNAME, ACCESS_KEY, JOB_ID, SERVER);
+        assertEquals(expectedLink, actualLink);
+    }
+
+    @Test
+    void testGetShareableLinkWithNullSauceJobId() {
+        assertThrows(IllegalArgumentException.class, () -> SauceShareableLink.getShareableLink("myUsername", "myAccessKey", null, "https://mydatacenter.saucelabs.com/"));
+    }
+
+    @Test
+    void testGetShareableLinkWithEmptySauceJobId() {
+        assertThrows(IllegalArgumentException.class, () -> SauceShareableLink.getShareableLink("myUsername", "myAccessKey", "", "https://mydatacenter.saucelabs.com/"));
+    }
+
+    @Test
+    void testGetShareableLinkWithNullDataCenterEndpoint() {
+        assertThrows(IllegalArgumentException.class, () -> SauceShareableLink.getShareableLink("myUsername", "myAccessKey", "12345", null));
+    }
+
+    @Test
+    void testGetShareableLinkWithEmptyDataCenterEndpoint() {
+        assertThrows(IllegalArgumentException.class, () -> SauceShareableLink.getShareableLink("myUsername", "myAccessKey", "12345", ""));
+    }
+
+    @Test
+    public void testGetShareableLinkWithDefaultUsername() throws NoSuchAlgorithmException, InvalidKeyException {
+        String defaultUsername = "myDefaultUsername";
+        System.setProperty("SAUCE_USERNAME", defaultUsername);
+
+        String accessKey = "myAccessKey";
+        String sauceJobId = "myJobId";
+        String dataCenterEndpoint = "https://my-datacenter.saucelabs.com/rest/v1/";
+
+        String expectedLink = "https://my-datacenter.saucelabs.com/rest/v1/tests/myJobId?auth=f0ea74946bbe5c0e531dedf5a4445d7a";
+
+        String actualLink = SauceShareableLink.getShareableLink(null, accessKey, sauceJobId, dataCenterEndpoint);
+
+        assertEquals(expectedLink, actualLink);
+    }
+
+    @Test
+    public void testGetShareableLinkWithDefaultAccessKey() throws NoSuchAlgorithmException, InvalidKeyException {
+        String defaultAccessKey = "myDefaultAccessKey";
+        System.setProperty("SAUCE_ACCESS_KEY", defaultAccessKey);
+
+        String username = "myUsername";
+        String sauceJobId = "myJobId";
+        String dataCenterEndpoint = "https://my-datacenter.saucelabs.com/rest/v1/";
+
+        String expectedLink = "https://my-datacenter.saucelabs.com/rest/v1/tests/myJobId?auth=1c6e5cb81e1bbb43869debe34d81c3fe";
+
+        String actualLink = SauceShareableLink.getShareableLink(username, null, sauceJobId, dataCenterEndpoint);
+
         assertEquals(expectedLink, actualLink);
     }
 }
