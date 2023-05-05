b55e4cd86775ad867cc2c75e5b5399ad26a34a71
sceiler <sceiler@users.noreply.github.com>
2023-05-05 23:25:52 +0200

    Make public fields private and add getter

--- src/main/java/com/saucelabs/saucerest/model/realdevices/Devices.java
+++ src/main/java/com/saucelabs/saucerest/model/realdevices/Devices.java
@@ -3,7 +3,11 @@ package com.saucelabs.saucerest.model.realdevices;
 import java.util.List;
 
 public class Devices {
-    public List<Device> deviceList;
+    public List<Device> getDeviceList() {
+        return deviceList;
+    }
+
+    private final List<Device> deviceList;
 
     public Devices(List<Device> devices) {
         super();
