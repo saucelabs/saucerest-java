02ab88951ad82b1632a43733925c7457720df1be
Diego Molina <diemol@gmail.com>
2023-10-19 15:32:12 +0200

    Adding US_EAST

--- src/main/java/com/saucelabs/saucerest/DataCenter.java
+++ src/main/java/com/saucelabs/saucerest/DataCenter.java
@@ -3,36 +3,49 @@ package com.saucelabs.saucerest;
 import java.util.stream.Stream;
 
 public enum DataCenter {
-    US_WEST("https://saucelabs.com/", "https://api.us-west-1.saucelabs.com/", "https://app.saucelabs.com/"),
-    EU_CENTRAL("https://eu-central-1.saucelabs.com/", "https://api.eu-central-1.saucelabs.com/", "https://app.eu-central-1.saucelabs.com/");
+  US_WEST(
+      "https://saucelabs.com/",
+      "https://api.us-west-1.saucelabs.com/",
+      "https://app.saucelabs.com/"),
+  US_EAST(
+      "https://saucelabs.com/",
+      "https://api.us-east-4.saucelabs.com/",
+      "https://app.us-east-4.saucelabs.com/"),
+  EU_CENTRAL(
+      "https://eu-central-1.saucelabs.com/",
+      "https://api.eu-central-1.saucelabs.com/",
+      "https://app.eu-central-1.saucelabs.com/");
 
-    public final String server;
-    public final String apiServer;
-    public final String appServer;
+  public final String server;
+  public final String apiServer;
+  public final String appServer;
 
-    DataCenter(String server, String apiServer, String appServer) {
-        this.server = server;
-        this.apiServer = apiServer;
-        this.appServer = appServer;
-    }
+  DataCenter(String server, String apiServer, String appServer) {
+    this.server = server;
+    this.apiServer = apiServer;
+    this.appServer = appServer;
+  }
 
-    public String server() {
-        return server;
-    }
+  public String server() {
+    return server;
+  }
 
-    public String apiServer() {
-        return apiServer;
-    }
+  public String apiServer() {
+    return apiServer;
+  }
 
-    public String edsServer() {
-        return apiServer + "v1/eds/";
-    }
+  public String edsServer() {
+    return apiServer + "v1/eds/";
+  }
 
-    public String appServer() {
-        return appServer;
-    }
+  public String appServer() {
+    return appServer;
+  }
 
-    public static DataCenter fromString(String dataCenter) {
-        return Stream.of(values()).filter(dc -> dc.name().equalsIgnoreCase(dataCenter)).findFirst().orElse(null);
-    }
+  public static DataCenter fromString(String dataCenter) {
+    return Stream.of(values())
+        .filter(dc -> dc.name().equalsIgnoreCase(dataCenter))
+        .findFirst()
+        .orElse(null);
+  }
 }
--- src/test/java/com/saucelabs/saucerest/unit/DataCenterTest.java
+++ src/test/java/com/saucelabs/saucerest/unit/DataCenterTest.java
@@ -9,65 +9,67 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 
 class DataCenterTest {
-    @ParameterizedTest
-    @CsvSource({
-        "US_WEST,        US_WEST",
-        "us_WeSt,        US_WEST",
-        "EU_CENTRAL,     EU_CENTRAL",
-        "Eu_central,     EU_CENTRAL",
-    })
-    void testFromString(String input, DataCenter expected) {
-        assertEquals(expected, DataCenter.fromString(input));
+  @ParameterizedTest
+  @CsvSource({
+    "US_WEST,        US_WEST",
+    "us_WeSt,        US_WEST",
+    "US_EAST,        US_EAST",
+    "us_EasT,        US_EAST",
+    "EU_CENTRAL,     EU_CENTRAL",
+    "Eu_central,     EU_CENTRAL",
+  })
+  void testFromString(String input, DataCenter expected) {
+    assertEquals(expected, DataCenter.fromString(input));
+  }
+
+  @Test
+  void testInvalidString() {
+    assertNull(DataCenter.fromString("unknown"));
+  }
+
+  @ParameterizedTest
+  @EnumSource(DataCenter.class)
+  void testEdsServer(DataCenter datacenter) {
+    assertTrue(datacenter.edsServer().endsWith("saucelabs.com/v1/eds/"));
+  }
+
+  @Test
+  void testFromStringNullAndEmpty() {
+    assertNull(DataCenter.fromString(null));
+    assertNull(DataCenter.fromString(""));
+  }
+
+  @Test
+  void testFromStringInvalid() {
+    assertNull(DataCenter.fromString("unknown"));
+    assertNull(DataCenter.fromString("random"));
+  }
+
+  @Test
+  void testEdsServerFormat() {
+    DataCenter dataCenter = DataCenter.US_WEST;
+    String edsServerUrl = dataCenter.edsServer();
+
+    assertTrue(edsServerUrl.startsWith("https://api.us-west-1.saucelabs.com/v1/eds/"));
+    assertTrue(edsServerUrl.endsWith("/v1/eds/"));
+  }
+
+  @Test
+  void testEdsServerNonEmpty() {
+    for (DataCenter dataCenter : DataCenter.values()) {
+      assertNotNull(dataCenter.edsServer());
+      assertFalse(dataCenter.edsServer().isEmpty());
     }
+  }
 
-    @Test
-    void testInvalidString() {
-        assertNull(DataCenter.fromString("unknown"));
-    }
+  @Test
+  void testServerUrls() {
+    assertEquals("https://saucelabs.com/", DataCenter.US_WEST.server());
+    assertEquals("https://api.us-west-1.saucelabs.com/", DataCenter.US_WEST.apiServer());
+    assertEquals("https://app.saucelabs.com/", DataCenter.US_WEST.appServer());
 
-    @ParameterizedTest
-    @EnumSource(DataCenter.class)
-    void testEdsServer(DataCenter datacenter) {
-        assertTrue(datacenter.edsServer().endsWith("saucelabs.com/v1/eds/"));
-    }
-
-    @Test
-    void testFromStringNullAndEmpty() {
-        assertNull(DataCenter.fromString(null));
-        assertNull(DataCenter.fromString(""));
-    }
-
-    @Test
-    void testFromStringInvalid() {
-        assertNull(DataCenter.fromString("unknown"));
-        assertNull(DataCenter.fromString("random"));
-    }
-
-    @Test
-    void testEdsServerFormat() {
-        DataCenter dataCenter = DataCenter.US_WEST;
-        String edsServerUrl = dataCenter.edsServer();
-
-        assertTrue(edsServerUrl.startsWith("https://api.us-west-1.saucelabs.com/v1/eds/"));
-        assertTrue(edsServerUrl.endsWith("/v1/eds/"));
-    }
-
-    @Test
-    void testEdsServerNonEmpty() {
-        for (DataCenter dataCenter : DataCenter.values()) {
-            assertNotNull(dataCenter.edsServer());
-            assertTrue(dataCenter.edsServer().length() > 0);
-        }
-    }
-
-    @Test
-    void testServerUrls() {
-        assertEquals("https://saucelabs.com/", DataCenter.US_WEST.server());
-        assertEquals("https://api.us-west-1.saucelabs.com/", DataCenter.US_WEST.apiServer());
-        assertEquals("https://app.saucelabs.com/", DataCenter.US_WEST.appServer());
-
-        assertEquals("https://eu-central-1.saucelabs.com/", DataCenter.EU_CENTRAL.server());
-        assertEquals("https://api.eu-central-1.saucelabs.com/", DataCenter.EU_CENTRAL.apiServer());
-        assertEquals("https://app.eu-central-1.saucelabs.com/", DataCenter.EU_CENTRAL.appServer());
-    }
+    assertEquals("https://eu-central-1.saucelabs.com/", DataCenter.EU_CENTRAL.server());
+    assertEquals("https://api.eu-central-1.saucelabs.com/", DataCenter.EU_CENTRAL.apiServer());
+    assertEquals("https://app.eu-central-1.saucelabs.com/", DataCenter.EU_CENTRAL.appServer());
+  }
 }
