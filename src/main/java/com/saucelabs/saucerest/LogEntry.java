5b54e2548194d34d039ad9fbf6146e0c9e2e3651
Diego Molina <diemol@gmail.com>
2024-05-27 15:22:42 +0200

    Fixing RDC log parsing

--- src/main/java/com/saucelabs/saucerest/LogEntry.java
+++ src/main/java/com/saucelabs/saucerest/LogEntry.java
@@ -1,30 +1,30 @@
 package com.saucelabs.saucerest;
 
 public class LogEntry {
-    private final String time;
-    private final String level;
-    private final String message;
+  private final String timestamp;
+  private final String level;
+  private final String message;
 
-    public LogEntry(String time, String level, String message) {
-        this.time = time;
-        this.level = level;
-        this.message = message;
-    }
+  public LogEntry(String time, String level, String message) {
+    this.timestamp = time;
+    this.level = level;
+    this.message = message;
+  }
 
-    public String getTime() {
-        return time;
-    }
+  public String getTime() {
+    return timestamp;
+  }
 
-    public String getLevel() {
-        return level;
-    }
+  public String getLevel() {
+    return level;
+  }
 
-    public String getMessage() {
-        return message;
-    }
+  public String getMessage() {
+    return message;
+  }
 
-    @Override
-    public String toString() {
-        return String.format("%s %s %s", time, level, message);
-    }
+  @Override
+  public String toString() {
+    return String.format("%s %s %s", timestamp, level, message);
+  }
 }
