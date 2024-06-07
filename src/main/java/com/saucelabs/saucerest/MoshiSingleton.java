15872bc4a677c45399d92d34134c568638db5c38
Valery Yatsynovich <valery_yatsynovich@epam.com>
2024-06-07 16:30:58 +0300

    Fix parsing of response with Tunnels Information

--- src/main/java/com/saucelabs/saucerest/MoshiSingleton.java
+++ src/main/java/com/saucelabs/saucerest/MoshiSingleton.java
@@ -1,6 +1,13 @@
 package com.saucelabs.saucerest;
 
+import java.io.IOException;
+import java.math.BigInteger;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.JsonReader;
+import com.squareup.moshi.JsonWriter;
 import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Moshi.Builder;
 
 public class MoshiSingleton {
 
@@ -11,7 +18,22 @@ public class MoshiSingleton {
 
     public static synchronized Moshi getInstance() {
         if (moshi == null) {
-            moshi = new Moshi.Builder().build();
+            moshi = new Builder()
+                .add(BigInteger.class, new JsonAdapter<BigInteger>()
+                {
+                    @Override
+                    public BigInteger fromJson(JsonReader jsonReader) throws IOException
+                    {
+                        return new BigInteger(jsonReader.nextString());
+                    }
+
+                    @Override
+                    public void toJson(JsonWriter jsonWriter, BigInteger bigInteger) throws IOException
+                    {
+                        jsonWriter.value(bigInteger.toString());
+                    }
+                })
+                .build();
         }
         return moshi;
     }
--- src/test/java/com/saucelabs/saucerest/unit/HelperTest.java
+++ src/test/java/com/saucelabs/saucerest/unit/HelperTest.java
@@ -12,34 +12,30 @@ class HelperTest {
     @Test
     void testGetResourceFileAsStringWithValidFile() throws IOException {
         // Test for valid file
-        Helper helper = new Helper();
         String fileName = "/testfile.txt";
         String expectedContent = "This is a test file.\nIt has multiple lines.";
-        String actualContent = helper.getResourceFileAsString(fileName);
+        String actualContent = Helper.getResourceFileAsString(fileName);
         assertEquals(expectedContent, actualContent);
     }
 
     @Test
     void testGetResourceFileAsStringWithNonexistentFile() {
         // Test for nonexistent file
-        Helper helper = new Helper();
         String fileName = "/nonexistentfile.txt";
-        assertThrows(NullPointerException.class, () -> helper.getResourceFileAsString(fileName));
+        assertThrows(NullPointerException.class, () -> Helper.getResourceFileAsString(fileName));
     }
 
     @Test
     void testGetResourceFileAsStringWithNullFileName() {
         // Test for null file name
-        Helper helper = new Helper();
         String fileName = null;
-        assertThrows(IllegalArgumentException.class, () -> helper.getResourceFileAsString(fileName));
+        assertThrows(IllegalArgumentException.class, () -> Helper.getResourceFileAsString(fileName));
     }
 
     @Test
     void testGetResourceFileAsStringWithEmptyFileName() {
         // Test for empty file name
-        Helper helper = new Helper();
         String fileName = "";
-        assertThrows(IllegalArgumentException.class, () -> helper.getResourceFileAsString(fileName));
+        assertThrows(IllegalArgumentException.class, () -> Helper.getResourceFileAsString(fileName));
     }
 }
